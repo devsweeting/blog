c60e903bb73dcac51d1cbdf793cda0227c381ef7
Devin Sweeting <devinsweeting@gmail.com>
2022-07-06 09:56:49 -0500

    create dockerfiles

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,11 +5,15 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 // The evenmt bus takes a single request and forwards it to all of the services.
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  // Posts service
+  events.push(event);
+
+  // Posts Service
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -21,7 +25,15 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+  //Moderation Service
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
